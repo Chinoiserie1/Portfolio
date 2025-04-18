c034fc5ab208a5f8dff38234127f1f2bf9403c1a
chixx <lucottejeremie@gmail.com>
2025-04-18 11:45:15 +0200

    update footer with full organisation name

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -34,8 +34,9 @@ export function Footer() {
                 <NavLink href="/uses">Uses</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Jérémie Lucotte. All rights
-                reserved.
+                &copy; {new Date().getFullYear()} LUCOTTE JEREMIE CHONG-HEI Sole
+                proprietorship registered in France under SIRET number 888 996
+                089 00015. All rights reserved.
               </p>
             </div>
           </ContainerInner>
